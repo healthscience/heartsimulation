3ba56897b3a8c7c3009c3c9446cf7880e3ae1503
Gavin Barnett <gavbarnett@gmail.com>
2017-11-26 16:34:19 +0000

    borked HeartTest2
    Its not working....but i didn't break anything

--- public/assets/js/Genetic Alg/HeartTest2.js
+++ public/assets/js/Genetic Alg/HeartTest2.js
@@ -1,57 +1,69 @@
 var Genetics = {
   $: {
+    GenerateFakeData: function(Number_of_samples){
+      var FakeData = [];
+      var XYZ = [];
+      var BPM_rest = 62;
+      var BPM_max = 190;
+      var Time = 0;
+      for (i = 0; i < Number_of_samples; i++) {
+        XYZ = [-100+Math.random()*200,-100+Math.random()*200,-100+Math.random()*200]
+        FakeData.push({
+          id:"TestSubject01",
+          xyz: XYZ,
+          bpm: (BPM_rest + ((Math.abs(XYZ[0]) + Math.abs(XYZ[2]) + Math.abs(XYZ[2]))/3)/100 * (BPM_max-BPM_rest)),
+          t: Time + 100
+        });
+      }
+      return FakeData;
+    },                      
     crossover: function(parent1, parent2){
       var child = {}
-      if (Math.random()>0.5) {
-        child.V = parent1.V
+      for (const key of Object.keys(parent1)) {
+        if (Math.random()>0.5) {
+          child[key] = parent1[key];
+        }
+        else {
+          child[key] = parent2[key];
+        }
       }
-      else {
-        child.V = parent2.V
-      }
-      if (Math.random()>0.5) {
-        child.F = parent1.F
-      }
-      else {
-        child.F = parent2.F
-      }
-      if (Math.random()>0.5) {
-        child.A = parent1.A
-      }
-      else {
-        child.A = parent2.A
-      }
-      child.V = Math.min(Math.max(child.V, 0.22),0.32)
-      child.F = Math.min(Math.max(child.F, 0.4),1.6)
-      child.A = Math.min(Math.max(child.A, 0.05),0.35)
       return(child)
     },
     mutate: function (solution){
-      if (Math.random()<0.3) {
-        solution.V = 0.22+Math.random()*0.1 //0.22 - 0.32L, 0.280L target
-      }
-      if (Math.random()<0.3) {
-        solution.F = 0.4+Math.random()*1.6 //0.4 - 1.6Hz, 1Hz target
-      }
-      if (Math.random()<0.3) {
-        solution.A = 0.05+Math.random()*0.3 //5% - 35%, 25% target
+      for (const key of Object.keys(solution)) {
+        if (Math.random()<0.3) {
+          solution[key] = solution[key] * (-1 + Math.random()*1) + (-1 + Math.random()*1); 
+        }
       }
       return(solution)
     },
-    getRandomSolution: function(){
-      var solution = {
-        V: 0.22+Math.random()*0.1, 
-        F: 0.4+Math.random()*1.6, 
-        A: 0.05+Math.random()*0.3  
+    getRandomModel: function(){
+      var Model = {};
+      for (i = 0; i < 20; i++) {
+        Model[i] = Math.random()*100;  
       }
-      return solution
+      return Model
     },
-    fitness: function(solution,target){
-      var targetflow = target;
-      var actualflow = solution.V*solution.F*solution.A;
-      return(Math.abs(actualflow-targetflow))
+    test: function(Model, input_data){
+      var output_data = [];
+      input_data.forEach(function(item){
+        output_data.push = item;
+        output_data.bpm = (
+          (output_data.xyz[0]*Model[0] + output_data.xyz[1]*Model[1] + output_data.xyz[2]*Model[2])/Model[3]*Model[4]
+        ); 
+      });
+      return(output_data)
+    },
+    fitness: function(Model, training_data){
+      var test_results = Genetics.$.test(Model, training_data);
+      var score = 0;
+      training_data.forEach(function(item){
+        score = Math.pow(score,2) + Math.pow(Math.abs(training_data.bpm - test_results.bpm),2);
+      });
+      return(score)
     }
   },
-  Train: function(input, pool, iterations, target){
+  Train: function(Input_Model, pool, iterations, training_data){
     //inputs are optional. 
   //input = {V:, F:, A:,Score: }
   //pool defaults to 1000
@@ -59,8 +71,8 @@ var Genetics = {
   var parents = [];
   var childs = [];
   var score = [];
-  if(!target){
-    target = 0.28*1*.25;
+  if(!training_data){
+    training_data = Genetics.$.GenerateFakeData(500);
   }
   if (!pool) {
     pool = 1000;
@@ -68,17 +80,24 @@ var Genetics = {
   if (!iterations) {
     iterations = 10;
   };
-  if(!input){
+  if(!Input_Model){
     for (i = 0; i < pool; i++) {
-      parents.push(Genetics.$.getRandomSolution());
-      parents[i].Score = fitness(parents[i], target);
+      tempmodel = Genetics.$.getRandomModel()
+      parents[i] = {
+        Model: tempmodel,
+        Score: Genetics.$.fitness(tempmodel, training_data)
+      };
     } 
   } else {
     for (i = 0; i < pool; i++) {
-      parents.push(Genetics.$.mutate(input));
-      parents[i].Score = Genetics.$.fitness(parents[i], target);
+      tempmodel = Genetics.$.mutate(Input_Model.Model)
+      parents[i] = {
+        Model: tempmodel,
+        Score: Genetics.$.fitness(tempmodel, training_data)
+      };
     } 
   }
+  Console.log(parents);
   for (n = 0; n <iterations; n++){
     //sort on best score
     parents.sort(function(a,b){
@@ -86,9 +105,9 @@ var Genetics = {
     })
     for (i = 0; i < pool; i++) {
       childs[i] = {};
-      childs[i] = Genetics.$.crossover(parents[Math.round(Math.random()*(pool/4),1)], parents[Math.round(Math.random()*(pool/4),1)]); 
-      childs[i] = Genetics.$.mutate(childs[i]);
-      childs[i].Score = Genetics.$.fitness(childs[i], target);
+      childs[i].Model = Genetics.$.crossover(parents[Math.round(Math.random()*(pool/4),1)].Model, parents[Math.round(Math.random()*(pool/4),1)].Model); 
+      childs[i].Model = Genetics.$.mutate(childs[i].Model);
+      childs[i].Score = Genetics.$.fitness(childs[i].Model, training_data);
     } 
     parents = childs;
   }
@@ -96,9 +115,10 @@ var Genetics = {
   parents.sort(function(a,b){
     return a.Score-b.Score;
   })
-  return parents[0];
+  var Best_Model = parents[0];
+  return Best_Model;
   },
-  Query: function(input){
-
+  Query: function(Model, input_data){
+    return Genetics.$.test(Model.Model,input_data);
   }
 };
